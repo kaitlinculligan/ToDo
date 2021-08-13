ca0928cfd3e3aad1603bd28a7c0b197a3c0b0857
kaitlinculligan <81047241+kaitlinculligan@users.noreply.github.com>
2021-08-13 13:18:27 -0600

    Update UI.java
    Added basic framework for GUI, requires further design work and decisions

--- UI.java
+++ UI.java
@@ -6,6 +6,9 @@
  * @author Kaitlin Culligan
  */
 
+ import java.awt.*;
+ import javax.swing.*;
+
 public class UI {
     
     private Profile profile;
@@ -24,6 +27,21 @@ public class UI {
     }
 
     public static void main(String[] args){
-         
+         EventQueue.invokeLater(() -> {
+             var frame = new SimpleFrame();
+             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+             frame.setVisible(true);
+             frame.setResizable(true);
+             frame.setTitle("ToDo");
+         });
+    }
+}
+
+class SimpleFrame extends JFrame{
+    private static final int DEFAULT_WIDTH = 300;
+    private static final int DEFAULT_HEIGHT = 200;
+
+    public SimpleFrame(){
+        setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
     }
 }
