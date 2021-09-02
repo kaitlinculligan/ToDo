88d509b11e0405728b7525aed3c8fde42997ade1
kaitlinculligan <81047241+kaitlinculligan@users.noreply.github.com>
2021-09-02 15:14:48 -0600

    Created UsernameLabel class

---
+++ UIElements/UsernameLabel.java
@@ -0,0 +1,25 @@
+/**
+ * The label for the username page used for the program as part of java swing
+ * 
+ * @version 1.0
+ * @since 1.0
+ * @author Kaitlin Culligan
+ */
+
+package UIElements;
+
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class UsernameLabel extends JLabel{
+    
+
+    public void setLabel(String label){
+        setLabel(label);
+    }
+
+    public String getLabel(){
+        return getLabel();
+    }
+}
